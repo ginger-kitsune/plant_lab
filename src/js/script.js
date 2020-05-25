87bbd6043f405143624634774e7e420cd3d08fc0
Victoria <iubirea5@yandex.ru>
2020-05-25 16:05:20 +0300

    add mobile adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,20 @@
 //Mask
 $('input[name=phone]').mask("+1 (999) 999-9999");
 
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__nav'),
+    menuItem = document.querySelectorAll('.header__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__nav_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__nav_active');
+        });
+    });
+});
